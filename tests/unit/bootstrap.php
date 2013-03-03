01710f1bc8afc53e381f257439e6abe935f77f0e
Ciaran McNulty <mail@ciaranmcnulty.com>
2013-03-03 16:25:33 +0000

    Whitespace changes

--- tests/unit/bootstrap.php
+++ tests/unit/bootstrap.php
@@ -1,9 +1,6 @@
 <?php
 
-
 date_default_timezone_set('Europe/London');
 
 require_once dirname(__FILE__) . '/../../classes/includes.php';
-
 require_once dirname(__FILE__) . '/classes/CliCommand/Meta/Abstract.php';
-
